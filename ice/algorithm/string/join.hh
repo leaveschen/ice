29bfc3c9a573b6bfc252c3713c446b5e714f1353
chenli.c <leaveschen@outlook.com>
2019-06-27 18:09:55 +0800

    update

--- ice/algorithm/string/join.hh
+++ ice/algorithm/string/join.hh
@@ -9,6 +9,36 @@
 
 /* class & function section */
 
+namespace ice {
+
+namespace algorithm {
+
+template<class Sequence, class DelimiterRange>
+decltype(auto) join(Sequence const& sequence,
+		DelimiterRange const& delimiter) {
+	// TODO: implement with traits
+	using result_type = typename Sequence::value_type;
+
+	result_type result;
+	auto sequence_it = std::begin(sequence);
+	auto sequence_end = std::end(sequence);
+
+	// insert first element
+	if (sequence_it != sequence_end) {
+		result.insert(std::end(result), std::begin(*sequence_it), std::end(*sequence_it));
+		++sequence_it;
+	}
+
+	for (; sequence_it != sequence_end; ++sequence_it) {
+		result.insert(std::end(result), std::begin(delimiter), std::end(delimiter));
+		result.insert(std::end(result), std::begin(*sequence_it), std::end(*sequence_it));
+	}
+	return result;
+}
+
+} // namespace algorithm
+
+} // namespace ice
 
 #endif//
 
--- unittest/algorithm/unittest_string.cc
+++ unittest/algorithm/unittest_string.cc
@@ -6,15 +6,35 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 #include <ice/algorithm/string.hh>
 
 /* class & function section */
 
-int main() {
+void test_predicate() {
 	std::string input{"xijiancuoha"};
-	std::string test{"xijian"};
-	assert(!ice::algorithm::startswith(input, test));
+	std::string test1{"xijian"};
+	std::string test2{"cuoha"};
+	std::cout << ice::algorithm::startswith(input, test1) << "\n";
+	std::cout << ice::algorithm::startswith(input, test2) << "\n";
+	std::cout << ice::algorithm::endswith(input, test1) << "\n";
+	std::cout << ice::algorithm::endswith(input, test2) << "\n";
+}
+
+void test_join() {
+	std::vector<std::string> sequence1{"ab", "cd", "ef", "gh"};
+	std::vector<std::string> sequence2{"ab"};
+	std::vector<std::string> sequence3{};
+	std::string delimiter{"-"};
+	std::cout << ice::algorithm::join(sequence1, delimiter) << "\n";
+	std::cout << ice::algorithm::join(sequence2, delimiter) << "\n";
+	std::cout << ice::algorithm::join(sequence3, delimiter) << "\n";
+}
+
+int main() {
+	test_predicate();
+	test_join();
 }
 
 
